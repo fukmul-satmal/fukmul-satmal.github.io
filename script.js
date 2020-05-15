176e9537c830c0f5c8bf27ad94275f501f98ff43
fukmul-satmal <sato_rin_sato_imo@yahoo.co.jp>
2020-05-15 14:48:56 +0900

    edit script.js

--- script.js
+++ script.js
@@ -49,8 +49,8 @@ const updateUI = async () => {
   document.getElementById("btn-login").disabled = isAuthenticated;
   
   // NEW - add logic to show/hide gated content after authentication
-  if (isAuthenticated) {
-    document.getElementById("gated-content").classList.remove("hidden");
+//  if (isAuthenticated) {
+//    document.getElementById("gated-content").classList.remove("hidden");
 
     document.getElementById(
       "ipt-access-token"
@@ -64,9 +64,9 @@ const updateUI = async () => {
     const profile = await auth0.getUser();
     document.getElementById("ipt-user-profile-image").src = profile.picture;
 
-  } else {
-    document.getElementById("gated-content").classList.add("hidden");
-  }
+//  } else {
+//    document.getElementById("gated-content").classList.add("hidden");
+//  }
 };
 
 const login = async () => {
